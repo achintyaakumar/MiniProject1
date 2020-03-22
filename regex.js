b7b42279659f7b47c7a75d556b8d3ff02c941269
Achintya Kumar <32974311+achintyaakumar@users.noreply.github.com>
2020-03-22 02:44:21 -0400

    updates

--- regex.js
+++ regex.js
@@ -1,1 +1,13 @@
-Achintya
+var result = document.querySelector('#result');
+var expression = document.querySelector('#input');
+var text = document.querySelector('#textarea');
+
+document.getElementById("button").addEventListener("click", highlight);
+
+function highlight(){
+    if (text.match(expression)) {
+    result.innerHTML = 'The phone number is <strong>valid!</strong>';
+  } else {
+    result.innerHTML = 'The phone number is <strong>invalid!</strong>';
+  }
+}
