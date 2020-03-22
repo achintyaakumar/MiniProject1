838c35bf9321f35a2ead89187dbac127d6baa676
Achintya Kumar <32974311+achintyaakumar@users.noreply.github.com>
2020-03-22 13:45:56 -0400

    added some css

--- regex.js
+++ regex.js
@@ -1,14 +1,16 @@
 /*eslint-env browser*/
 document.getElementById("button").addEventListener("click", highlight);
+var iden = 'light';
 
 function highlight() {
     var result = document.querySelector('#result');
     var expression = document.getElementById('input').value;
     var text = document.getElementById('textarea').value;
-    alert(expression);
-    if (text.match(expression)) {
-        result.innerHTML = 'The phone number is <strong>valid!</strong>';
-  } else {
-        result.innerHTML = 'The phone number is <strong>invalid!</strong>';
-  }
-}
+    var matches = text.match(expression);
+    
+    // verify it's alphanumeric and strings up to fifty characters
+    //if (!matches.match(/^[0-9a-zA-Z]{1,50}$/)) return;
+    
+    var alter = '<span ' + (iden ? 'class="' + iden + '"' : '') + '>' + text + '</span>';
+    result.innerHTML = text.replace(text, alter);
+} 
