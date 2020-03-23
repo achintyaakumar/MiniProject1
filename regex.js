c6731e3d369601f6861fb2b6c50c133a52b89de6
Achintya Kumar <32974311+achintyaakumar@users.noreply.github.com>
2020-03-23 16:49:31 -0400

    working js

--- regex.js
+++ regex.js
@@ -1,16 +1,29 @@
 /*eslint-env browser*/
-document.getElementById("button").addEventListener("click", highlight);
-var iden = 'light';
+//document.getElementById("button").addEventListener("click", highlight);
 
 function highlight() {
     var result = document.querySelector('#result');
-    var expression = document.getElementById('input').value;
+    var expression = new RegExp(document.getElementById('input').value, 'g');
     var text = document.getElementById('textarea').value;
-    var matches = text.match(expression);
+    text = text.split("\n").join("<br>");
+//  var matches = text.matchAll(expression);
+//  verify it's alphanumeric and strings up to fifty characters
+//  if (!matches.match(/^[0-9a-zA-Z]{1,50}$/)) return; 
+    result.innerHTML = "";
+    var matches = Array.from(expression[Symbol.matchAll](text));
+    var wI, sI;
     
-    // verify it's alphanumeric and strings up to fifty characters
-    //if (!matches.match(/^[0-9a-zA-Z]{1,50}$/)) return;
-    
-    var alter = '<span ' + (iden ? 'class="' + iden + '"' : '') + '>' + text + '</span>';
-    result.innerHTML = text.replace(text, alter);
+    for(var match of matches) {
+        wI = text.indexOf(match, sI);
+        result.innerHTML+=text.substring(sI, wI);
+        result.innerHTML+="<span>" + match + "</span>";
+        sI=wI+match.length;
+    }
+    result.innerHTML+=text.substring(sI);
+     
+//  for (var match of matches) {
+//      alert(match);
+//      var alter = "<span>" + match + "</span>"";
+//      result.innerHTML = result.split(match).join(alter);
+//    }  
 } 
